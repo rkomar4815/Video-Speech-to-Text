601c19a1fa3aad936759009c96f2d07e1b8b0541
Rao Komar <Raokomar@gmail.com>
2019-06-28 00:18:07 -0400

    created main function for speech.py

--- speech/config.py
+++ speech/config.py
@@ -1,8 +1,7 @@
-outputfilename = None
+filename = None
 project = 'committeehearingtotext'
 bucket = 'hearingtotext'
 gcs_uri = None
 words = None
 gcred = 'committeehearingtotext-bd8120cea906.json'
-URL = None
 speakernum = None
