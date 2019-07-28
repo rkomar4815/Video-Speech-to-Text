959b761e035666566bc56a2c47a309f6d69346f5
Rao Komar <Raokomar@gmail.com>
2019-07-28 16:45:56 -0400

    add transcript function for single speaker

--- speech/speech.py
+++ speech/speech.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 from google.cloud import storage
 from pydub import AudioSegment
-from google.cloud import speech_v1p1beta1 as speech
-from google.cloud.speech_v1p1beta1 import enums
-from google.cloud.speech_v1p1beta1 import types
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+from google.cloud import speech_v1p1beta1
+from google.cloud.speech_v1p1beta1 import enums as beta_enums
+from google.cloud.speech_v1p1beta1 import types as beta_types
 from google.protobuf.json_format import MessageToDict
 import youtube_dl
 import config
@@ -13,14 +16,17 @@ import pandas as pd
 import sys
 
 '''
-This module converts any youtube or soundcloud video/audio file into a text file transcript with unique speakers identified.
+This module converts any youtube video to a transcript
 
-The GCP Speech to Text AI used in this module is diarization enabled meaning it will identify unique speakers in audio.
-However, GCP's diarized Speech to Text AI is in beta, so there may be some bugs when long videos are analyzed.
+The GCP Speech to Text AI used in this module is diarization enabled meaning
+it will identify unique speakers in audio.
+
+However, GCP's diarized Speech to Text AI is in beta,
+so there may be some bugs when long videos are analyzed.
 '''
 
 
-def main(url, speakernum):
+def main(url, speakernum=1):
 
     speakernum = int(speakernum)
 
@@ -35,9 +41,18 @@ def main(url, speakernum):
 
     os.remove(config.filename)
 
-    config.words = videotranscribe(config.gcs_uri, speakernum)
+    if speakernum != 1:
 
-    _transcript_maker(config.words, config.filename)
+        config.words = diarized_transcribe(
+            config.gcred, config.gcs_uri, speakernum)
+
+        _diarized_transcript_maker(config.words, config.filename)
+
+    else:
+
+        config.words = standard_transcribe(config.gcred, config.gcs_uri)
+
+        _standard_transcript_maker(config.words, config.filename)
 
     delete_blob(
         config.gcred, config.project,
@@ -48,7 +63,7 @@ def main(url, speakernum):
 # Hooks for Youtube-dl
 # Youtube videos are either downloaded as .webm files or .m4a files
 
-def _my_hook(d):  # changes filename string from .webm to .flac
+def _my_hook(d):  # changes filename from .webm to .flac post conversion
     if d['status'] == 'finished':
         filename = str(d['filename'])
         if '.webm' in str(filename):
@@ -91,7 +106,7 @@ def stereo_to_mono(filename):
     return newfilename
 
 #  The function below uses PyDub to remove silent chunks from the audio file
-#  Note that this only removes silent chunks from the start and end of audio file
+#  Note: only removes silent chunks from the start & end of audio file
 
 
 def silence_trim(outputfilename):
@@ -135,16 +150,18 @@ def gcloud_uploader(gcred, project, bucketname, outputfilename):
     return gcs_uri
 
 
-#  Transcribes an audio file based on a passed through speakercount and GCS URI
+#  Transcribes audio based on a passed through speakercount and GCS URI
+#  Results in a diarized transcript with unique speakers identified
 
 
-def videotranscribe(gcs_uri, speakercount):
+def diarized_transcribe(gcred, gcs_uri, speakercount):
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = gcred
 
-    client = speech.SpeechClient()
-    audio = types.RecognitionAudio(uri=gcs_uri)
+    client = speech_v1p1beta1.SpeechClient()
+    audio = beta_types.RecognitionAudio(uri=gcs_uri)
 
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+    config = beta_types.RecognitionConfig(
+        encoding=beta_enums.RecognitionConfig.AudioEncoding.FLAC,
         language_code='en-US',
         enable_speaker_diarization=True,
         diarization_speaker_count=speakercount,
@@ -155,45 +172,82 @@ def videotranscribe(gcs_uri, speakercount):
 
     operation = client.long_running_recognize(config, audio)
 
-    response = operation.result(timeout=30000)
+    response = operation.result(timeout=3600)
 
-    words = MessageToDict(response)
+    transcript = MessageToDict(response)
 
-    words = words.get('results')
-    words = words.pop()
-    words = words.get('alternatives')
-    words = words.pop()
-    words = words.get('words')
+    transcript = transcript.get('results')
+    transcript = transcript.pop()
+    transcript = transcript.get('alternatives')
+    transcript = transcript.pop()
+    transcript = transcript.get('words')
 
-    return words
+    return transcript
 
 
-def _transcript_maker(words, filename):
-    words_df = pd.DataFrame(words)
+def _diarized_transcript_maker(transcript, filename):
 
-    if 'speakerTag' in words_df:
+    words_df = pd.DataFrame(transcript)
 
-        words_df['current_speaker'] = (
-            words_df.speakerTag.shift() != words_df.speakerTag).cumsum()
+    pd.set_option('max_colwidth', 50000)  # Stops transcript truncation
 
-        transcript_df = words_df.groupby('current_speaker').agg({
-            'startTime': min,
-            'speakerTag': min,
-            'word': lambda x: ' '.join(x),
-            'endTime': max
-        }).rename(columns={'word': 'transcript'})
+    words_df['current_speaker'] = (
+        words_df.speakerTag.shift() != words_df.speakerTag).cumsum()
 
-        textfilename = filename.replace('_mono.flac', '.txt')
+    transcript_df = words_df.groupby('current_speaker').agg({
+        'startTime': min,
+        'speakerTag': min,
+        'word': lambda x: ' '.join(x),
+        'endTime': max
+    }).rename(columns={'word': 'transcript'})
 
-        with open(textfilename, 'a') as file:
+    textfilename = filename.replace('_mono.flac', '.txt')
 
-            file.write(transcript_df.to_string(
-                index=False,
-                columns=['startTime', 'speakerTag', 'transcript'],
-                header=False, formatters=(
-                    {'startTime': '\n StartTime: {:} \n'.format,
-                        'speakerTag': '\n Speaker {:}: \n'.format,
-                        'transcript': '\n {:} \n'.format})))
+    with open(textfilename, 'a') as file:
+
+        file.write(transcript_df.to_string(
+            index=False,
+            columns=['startTime', 'speakerTag', 'transcript'],
+            header=False, formatters=(
+                {'startTime': '\n StartTime: {:} \n'.format,
+                    'speakerTag': '\n Speaker {:}: \n'.format,
+                    'transcript': '\n {:} \n'.format})))
+
+
+# Transcribes an audio file with only one speaker and no punctuation
+
+def standard_transcribe(gcred, gcs_uri):
+
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = gcred
+
+    transcript = ''
+
+    client = speech.SpeechClient()
+    audio = types.RecognitionAudio(uri=gcs_uri)
+
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        language_code='en-US')
+
+    operation = client.long_running_recognize(config, audio)
+
+    response = operation.result(timeout=3600)
+
+    for result in response.results:
+        transcript += result.alternatives[0].transcript
+
+    return transcript
+
+
+# Creates a standard transcript with only one speaker
+
+def _standard_transcript_maker(transcript, filename):
+
+    textfilename = filename.replace('_mono.flac', '.txt')
+
+    with open(textfilename, 'a') as file:
+
+        file.write(transcript)
 
 
 #  Deletes a file in Google Cloud blob storage
@@ -202,7 +256,7 @@ def delete_blob(gcred, project, bucket, outputfilename):
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = gcred
 
     storage_client = storage.Client(project=project)
-    bucket = storage_client.get_bucket(bucket)  # your bucket name
+    bucket = storage_client.get_bucket(bucket)
     blob = bucket.blob(outputfilename)
 
     blob.delete()
